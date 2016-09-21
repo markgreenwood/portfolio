45336ee4360772a92944e6d360a1d4a8fe915d47
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-09-20 21:00:44 -0700

    Now reading article content from simulated backend; slight aesthetic improvements

--- scripts/articles.js
+++ scripts/articles.js
@@ -8,9 +8,34 @@ function Article(opts) {
 }
 
 Article.prototype.toHtml = function () {
-  
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+
+  /* TODO: Need to fill in rest of template clone with properties from this
+  Article instance.
+  Need:
+    1. title
+    2. publication date
+    3. body
+  */
+  $newArticle.find('.article-title').text(this.title);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('.article-body').html(this.body);
+
+  $newArticle.removeClass('template');
+
+  return $newArticle;
 };
 
 ourLocalData.sort(function(curElem, nextElem) {
   return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a) {
+  $('#articles').append(a.toHtml());
 });
