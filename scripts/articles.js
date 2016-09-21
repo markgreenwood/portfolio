591ffdf73969a5321efbd041bccc92a062cbab94
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-09-20 20:26:08 -0700

    Got icons working (including new icons via icomoon app); created constructor function for Article in articles.js

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,11 +1,14 @@
 var articles = [];
 
 function Article(opts) {
-
+  this.title = opts.title;
+  this.category = opts.category;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
 }
 
 Article.prototype.toHtml = function () {
-
+  
 };
 
 ourLocalData.sort(function(curElem, nextElem) {
