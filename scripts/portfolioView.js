ac92c642dff76c61050e4f3d823e42687b6d5948
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-09-29 16:10:52 -0700

    Encapsulated view .js file in IIFE to limit exposure.

--- scripts/portfolioView.js
+++ scripts/portfolioView.js
@@ -1,27 +1,29 @@
-var portfolioView = {};
+(function(module) {
+  var portfolioView = {};
 
-portfolioView.handleMainNav = function() {
-  /*
-  */
-  $('.main-nav').on('click', '.tab', function() {
-    var selection = $(this).attr('data-content');
-    //console.log(selection);
-    $('.tab-content').hide();
-    $('#' + selection).fadeIn('slow');
-  });
+  portfolioView.handleMainNav = function() {
+    /*
+    */
+    $('.main-nav').on('click', '.tab', function() {
+      var selection = $(this).attr('data-content');
+      //console.log(selection);
+      $('.tab-content').hide();
+      $('#' + selection).fadeIn('slow');
+    });
 
-  $('.main-nav .tab:first').click();
-};
+    $('.main-nav .tab:first').click();
+  };
 
-portfolioView.renderIndexPage = function() {
-  Article.all.forEach(function(article) {
-    $('#articles').append(article.toHtml());
-  });
-  portfolioView.handleMainNav();
-  $('#num-articles').text(Article.numArticles());
-  $('#avg-word-count').text(Article.avgWordCount());
-  // TODO: report avg post age
-  $('#avg-post-age').text(Article.avgPostAge());
-};
+  portfolioView.renderIndexPage = function() {
+    Article.all.forEach(function(article) {
+      $('#articles').append(article.toHtml());
+    });
+    portfolioView.handleMainNav();
+    $('#num-articles').text(Article.numArticles());
+    $('#avg-word-count').text(Article.avgWordCount());
+    // TODO: report avg post age
+    $('#avg-post-age').text(Article.avgPostAge());
+  };
 
-Article.fetchAll(portfolioView.renderIndexPage);
+  Article.fetchAll(portfolioView.renderIndexPage);
+})();
