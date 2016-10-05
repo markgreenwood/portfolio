032513603804aafd61bf4efcca5822cfffd11d22
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-05 15:52:47 -0700

    Changed AJAX call for GitHub repos and added code for token handling from environment variable in server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ var express = require('express'),
 function proxyGitHub(request, response) {
   console.log('Routing Github request for', request.params[0]);
   (requestProxy({
-    url: 'https://api.github.com/' + request.params[0]
+    url: 'https://api.github.com/' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
   }))(request, response);
 };
 
